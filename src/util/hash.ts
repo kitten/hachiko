dbe2acb666586cc49094f67052fec9f57ef471e4
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2016-11-22 03:18:25 +0000

    Inline smi method into hash

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,7 +1,3 @@
-const smi = (x: number) => (
-  ((x >>> 1) & 0x40000000) | (x & 0xBFFFFFFF)
-)
-
 // Turning strings to (32-bit) number hashes
 // See: http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
 const hash = (x: number | string): number => {
@@ -16,7 +12,7 @@ const hash = (x: number | string): number => {
     hash = 31 * hash + x.charCodeAt(i)
   }
 
-  return smi(hash)
+  return ((hash >>> 1) & 0x40000000) | (hash & 0xBFFFFFFF)
 }
 
 export default hash
