30f0c622a39bdcb8f238c453de573f23a90f5cca
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2016-11-26 01:35:48 +0000

    Cut all hash numbers to int32

--- src/util/hash.ts
+++ src/util/hash.ts
@@ -1,8 +1,12 @@
+const toInt32 = (x: number) => (
+  ((x >>> 1) & 0x40000000) | (x & 0xBFFFFFFF)
+)
+
 // Turning strings to (32-bit) number hashes
 // See: http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
 const hash = (x: number | string): number => {
   if (typeof x === 'number') {
-    return x
+    return toInt32(x)
   }
 
   const length = x.length
@@ -12,7 +16,7 @@ const hash = (x: number | string): number => {
     hash = 31 * hash + x.charCodeAt(i)
   }
 
-  return ((hash >>> 1) & 0x40000000) | (hash & 0xBFFFFFFF)
+  return toInt32(hash)
 }
 
 export default hash
