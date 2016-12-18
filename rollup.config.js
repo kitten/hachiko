8b0894e2051332203ae8017047b222a43e197901
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2016-12-18 22:13:07 +0000

    Add better tslib handling and UMD minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'typescript'
 import rollupAlias from "rollup-plugin-alias"
 import rollupTypescript from 'rollup-plugin-typescript'
+import rollupCleanup from 'rollup-plugin-cleanup'
 
 export default {
   entry: './src/index.ts',
@@ -8,9 +9,12 @@ export default {
     rollupAlias({
       tslib: "node_modules/tslib/tslib.es6.js",
     }),
+    rollupCleanup({
+      comments: 'none',
+      maxEmptyLines: 1
+    }),
     rollupTypescript({
-      importHelpers: true,
-      noEmitHelpers: true,
+      declaration: false,
       typescript
     })
   ]
